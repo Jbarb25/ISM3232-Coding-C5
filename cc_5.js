e0f97c8d3309f0b148d570a8021eed07ab9ab84e
Jbarb25 <barboza205@usf.edu>
2025-02-05 15:58:46 -0500

    Task 2 - Order Details
    Declaring the object with the properties. Adding the displayOrder method that logs the order details using "this." Calls the displayOrder method and logs the details from the function.

--- cc_5.js
+++ cc_5.js
@@ -7,3 +7,16 @@ let customer = {            //declaring an object
 console.log(`Customer Name: ${customer.name}`)  //logging each property using template literal
 console.log(`Customer Age: ${customer.age}`)
 console.log(`Customer Email: ${customer.email}`)
+
+
+//Task 2: Object Methods
+let order = {      //Declaring the object order with the properties
+    orderID: 12345,
+    totalAmount: 150,
+    status: "Processing",
+    displayOrder: function(){    //Adding the displayOrder method that logs the order details using "this"
+        console.log(`Order ID: ${this.orderID}`),
+        console.log(`Total Amount: ${this.totalAmount}`), 
+        console.log(`Order Status: ${this.status}`)}}
+
+order.displayOrder()  //Calls the displayOrder method and logs the details from the function
