b6cad063e9e792ab9fb08caf1b8762eb615be174
Jbarb25 <barboza205@usf.edu>
2025-02-05 15:38:12 -0500

    Task 1 - Customer Profile
    Declaring an object and logging each property using template literal.

---
+++ cc_5.js
@@ -0,0 +1,9 @@
+//Task 1: Object Properties
+let customer = {            //declaring an object
+    name: "Mickey Mouse",
+    age: 42,
+    email: "mickeymouse@disney.com"}
+
+console.log(`Customer Name: ${customer.name}`)  //logging each property using template literal
+console.log(`Customer Age: ${customer.age}`)
+console.log(`Customer Email: ${customer.email}`)
