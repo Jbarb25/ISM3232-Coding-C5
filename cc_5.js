fc86beb8c9148ebb939aa9568d371415518a2ef1
Jbarb25 <barboza205@usf.edu>
2025-02-05 16:10:01 -0500

    Task 3 - Shopping Cart
    Add a new product to the end of the array and then remove the last item. Add a new product to the beginning of the array and then remove the first product. Print the array of products to the console.

--- cc_5.js
+++ cc_5.js
@@ -20,3 +20,13 @@ let order = {      //Declaring the object order with the properties
         console.log(`Order Status: ${this.status}`)}}
 
 order.displayOrder()  //Calls the displayOrder method and logs the details from the function
+
+
+//Task 3: Array Manipulation (push, pop, shift, unshift)
+let cartItems = ["hat", "shoes", "sunglasses"]   //Delcare an array with three products
+
+cartItems.push("shirt")  //Add a new product to the cart using .push
+cartItems.pop()  //Remove the last item using .pop that will return the item if requested
+cartItems.unshift("jacket")  //Add an item at the beginning using .unshift 
+cartItems.shift()  //Remove the first item using .shift and returns it if requested
+console.log(cartItems)
